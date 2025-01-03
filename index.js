5fe3980a1b63fd5a4c1afd42008f4e7abfd65289
atoolate <alexdelaet00@gmail.com>
2025-01-03 19:05:29 +0100

    feat: simplify MongoDB connection options for cleaner code

--- index.js
+++ index.js
@@ -8,10 +8,7 @@ const mongoose = require('mongoose');
 
 
 // MongoDB connection
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGODB_URI, {})
 .then(() => console.log('MongoDB connected successfully'))
 .catch(err => console.error('MongoDB connection error:', err));
 
